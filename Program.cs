8764f7bbc13f7b3d6177d8a372892b80a6fe11dd
wahyu9kdl <blanzcye.yud@gmail.com>
2026-06-24 12:47:03 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AlhikmahService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alhikmah Website Service in C#");
+        }
+    }
+}
